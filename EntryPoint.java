89999f05160e3a41452a15d67b0a3717ba3c5872
FranciscoEderCabreraParedes <francisco.cabrera@digitalfemsa.com>
2024-05-15 09:31:39 -0600

    Cambios destinados para PR lab

--- EntryPoint.java
+++ EntryPoint.java
@@ -1,5 +1,5 @@
 public class EntryPoint{
 	public static void main(String[] args) {
-		System.out.println("Mensaje principal");
+		System.out.println("Mensaje para lab 01 PR");
 	}
 }
--- EntryPointDev1.java
+++ EntryPointDev1.java
@@ -1,5 +1,5 @@
 public class EntryPoint{
 	public static void main(String[] args) {
-		System.out.println("Cambio desde DEV01");
+		System.out.println("Mensaje para lab 01 PR");
 	}
 }
