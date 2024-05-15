5385d7eed3b452f64c673480b1c646fced6d2ecd
FranciscoEderCabreraParedes <francisco.cabrera@digitalfemsa.com>
2024-05-14 18:22:04 -0600

    primer commit trunk based development

--- EntryPoint.java
+++ EntryPoint.java
@@ -1,5 +1,5 @@
 public class EntryPoint{
 	public static void main(String[] args) {
-		System.out.println("Primer commit sobre Trunk Based Development");
+		System.out.println("Primer commit sobre trunk based development");
 	}
 }
