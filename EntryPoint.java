925c22ca1aef4496112306cd9ed169348c66d769
FranciscoEderCabreraParedes <francisco.cabrera@digitalfemsa.com>
2024-05-14 19:00:03 -0600

    Commit conflict from dev01

--- EntryPoint.java
+++ EntryPoint.java
@@ -1,5 +1,5 @@
 public class EntryPoint{
 	public static void main(String[] args) {
-		System.out.println("Tercer commit sobre trunk based development");
+		System.out.println("Mensaje principal");
 	}
 }
